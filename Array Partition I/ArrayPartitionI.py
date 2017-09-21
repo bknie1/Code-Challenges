b032d93733c5a4690485216fcd74ece06107d024
bknie1 <brandonknieriem26@gmail.com>
2017-09-20 20:30:37 -0400

    Array Partition I

--- Array Partition I/ArrayPartitionI.py
+++ Array Partition I/ArrayPartitionI.py
@@ -11,18 +11,22 @@ def arrayPairSum(nums):
     # This takes the whole string ([::])
     # And it takes every other character (2)
     """
-    # Sorts the string but only keeps every other value.
-    # The last slice parameter is called a stride (2 here).
+    # Sorts the string.
+    # Slices off every other value using the stride slice arg.
     # This emulates what would have been our min pairing.
     # The left value is always less than the right.
     # So we grab the first element from the first two would be pairs.
-    nums = sorted(nums[::2])
-    return sum(nums)
+    nums = sorted(nums)
+    nums = nums[::2]
+    min_sum = sum(nums)
+    return min_sum
 
 # MAIN
 
 TEST_1 = [1, 4, 3, 2]   # O: 4 = 1 + 3
 TEST_2 = [10, 8, 6, 15] # O: 16 = 10 + 6
+TEST_3 = [1, 2, 3, 2]      # O: 1 + 3 = 4
 
-print(arrayPairSum(TEST_1))
-print(arrayPairSum(TEST_2))
+# print(arrayPairSum(TEST_1))
+# print(arrayPairSum(TEST_2))
+print(arrayPairSum(TEST_3))
