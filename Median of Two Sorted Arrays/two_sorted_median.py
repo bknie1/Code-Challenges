1d5a17a84c7237560ceb5ef9fd3ebdb0f2fe6935
bknie1 <brandonknieriem26@gmail.com>
2017-09-29 13:40:39 -0400

    Median of Two Sorted Arrays: log(n + m) progress.

--- Median of Two Sorted Arrays/two_sorted_median.py
+++ Median of Two Sorted Arrays/two_sorted_median.py
@@ -9,7 +9,8 @@ Example 1:
     nums2 = [2]
 
     The median is 2.0
-    Example 2:
+
+Example 2:
     nums1 = [1, 2]
     nums2 = [3, 4]
 
@@ -23,6 +24,37 @@ Ideas:
 
 # METHODS
 
+def find_median_of_sorted(nums_1, nums_2):
+    """
+    :type nums1: List[int]
+    :type nums2: List[int]
+    :rtype: float
+    """
+    mid_1 = len(nums_1) // 2
+    mid_2 = len(nums_2) // 2
 
+    print("List 1:", nums_1, "\t\tMid Index 1:", mid_1, "\t\tMid:", nums_1[mid_1])
+    print("List 2:", nums_2, "\t\tMid Index 2:", mid_2, "\t\tMid:", nums_2[mid_2], "\n")
+
+    median = (nums_1[mid_1] + nums_2[mid_2]) / 2
+    print("Potential Median:", median)
+
+    if mid_1 == 0 or mid_2 == 0:
+        print("Median:", median)
+        return median
+
+    if nums_1[mid_1] < nums_2[mid_2]:
+        print("mid 1 < mid 2")
+        find_median_of_sorted(nums_1[mid_1:], nums_2[:mid_2])
+    else:
+        print("mid 1 > mid 2")
+        find_median_of_sorted(nums_2[mid_2:], nums_1[mid_1:])
 
 # MAIN
+
+NUMS_1 = [1, 3]
+NUMS_2 = [2]
+NUMS_3 = [1, 2]
+NUMS_4 = [3, 4]
+
+print(find_median_of_sorted(NUMS_3, NUMS_4))
