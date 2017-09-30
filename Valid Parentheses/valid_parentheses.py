2f90c3a1087eab3a8e9406f4fff87895c6548ba0
bknie1 <brandonknieriem26@gmail.com>
2017-09-30 04:40:49 +0100

    "O(1)"

--- Valid Parentheses/valid_parentheses.py
+++ Valid Parentheses/valid_parentheses.py
@@ -11,6 +11,8 @@ def is_valid(test):
     :rtype: bool
     Replaces all valid pairings with empty strings.
     If there's a remaining length it means there was an invalid pair.
+    True = remaining length, invalid.
+    False = No remaining length, valid.
     """
     length = len(test)
     while length != 0 and length % 2 == 0:
@@ -22,13 +24,16 @@ def is_valid(test):
             length = len(test)
         else:               # If no change, then we're out of text.
             length = 0
-        return True         # Everything (valid pairs) replaced, nothing left.
-    return False            # There's a remaining length. Not everything was replaced.
+    return not bool(len(test)) # Remaining length determines validity.
 
 TEST_1 = "(){}[]{}()[]"
 TEST_2 = "()[]{]}([})"
 TEST_3 = "([])"
+TEST_4 = "(("
+TEST_5 = "["
 
 print(is_valid(TEST_1))
 print(is_valid(TEST_2))
 print(is_valid(TEST_3))
+print(is_valid(TEST_4))
+print(is_valid(TEST_5))
