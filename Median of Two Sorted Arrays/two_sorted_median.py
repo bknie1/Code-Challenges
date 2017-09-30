9ec871f677c3f8307d253b96276ed2223d716f17
bknie1 <brandonknieriem26@gmail.com>
2017-09-30 11:05:41 -0400

    "Starting over."

--- Median of Two Sorted Arrays/two_sorted_median.py
+++ Median of Two Sorted Arrays/two_sorted_median.py
@@ -17,12 +17,9 @@ Example 2:
     The median is (2 + 3)/2 = 2.5
 
 Ideas:
-    Find the median of both. This is our range because the median will be
-    somewhere between the two values. Recursively call and walk towards one
-    another. Direction depends on which array has the greater value.
+    Cut each array into two halves and use max/min to find the median.
 """
-
-# METHODS
+# METHODS ######################################################################
 
 def find_median_of_sorted(nums_1, nums_2):
     """
@@ -30,31 +27,25 @@ def find_median_of_sorted(nums_1, nums_2):
     :type nums2: List[int]
     :rtype: float
     """
-    mid_1 = len(nums_1) // 2
-    mid_2 = len(nums_2) // 2
 
-    print("List 1:", nums_1, "\t\tMid Index 1:", mid_1, "\t\tMid:", nums_1[mid_1])
-    print("List 2:", nums_2, "\t\tMid Index 2:", mid_2, "\t\tMid:", nums_2[mid_2], "\n")
+    return walk_lists()
+################################################################################
+def walk_lists():
+    """
+    Walk the two lists recursively to find the median.
+    """
+    # Break out condition.
 
-    median = (nums_1[mid_1] + nums_2[mid_2]) / 2
-    print("Potential Median:", median)
+    # Walk condition.
 
-    if mid_1 == 0 or mid_2 == 0:
-        print("Median:", median)
-        return median
-
-    if nums_1[mid_1] < nums_2[mid_2]:
-        print("mid 1 < mid 2")
-        find_median_of_sorted(nums_1[mid_1:], nums_2[:mid_2])
-    else:
-        print("mid 1 > mid 2")
-        find_median_of_sorted(nums_2[mid_2:], nums_1[mid_1:])
-
-# MAIN
+    return # median
+# MAIN #########################################################################
 
 NUMS_1 = [1, 3]
 NUMS_2 = [2]
 NUMS_3 = [1, 2]
 NUMS_4 = [3, 4]
+NUMS_5 = [1, 2, 4, 7, 9]
+NUMS_6 = [3, 5, 6, 8]
 
-print(find_median_of_sorted(NUMS_3, NUMS_4))
+print(find_median_of_sorted(NUMS_2, NUMS_3))
