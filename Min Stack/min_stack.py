98755a250a3a81bcf43e7abd8b32e3290f908ff2
bknie1 <brandonknieriem26@gmail.com>
2017-10-02 17:18:13 -0400

    +500 ms. Needs work. Min value interaction.

--- Min Stack/min_stack.py
+++ Min Stack/min_stack.py
@@ -1,39 +1,55 @@
 """A simple Min Stack implementation."""
+# pylint: disable=C0103
 class MinStack(object):
     """
     A simple Min Stack implementation:
     push(item)
     pop()
     top()
-    get_min()
+    getMin()
     """
     def __init__(self):
         """
         Initializes the stack and maintains a length value for navigation.
         """
         self.stack = []
+        self.length = len(self.stack)
+        self.current_min = 0
 ################################################################################
 
     def push(self, item):
         """
         Pushes an arg value on to the stack.
-        :type item: int
+        :type x: int
         :rtype: void
         """
         print("Pushing", item)
-        current_min = self.get_min()
+
+        current_min = self.getMin()     # Is the new item a min value?
         if current_min is None or item < current_min:
-            current_min = item
-        self.stack.append(item)
+            self.current_min = item
+
+        self.stack.append((item))       # Add to stack regardless.
+        self.length = len(self.stack)   # Update length using new data.
 ################################################################################
 
     def pop(self):
         """
-        Pops the top value off the stack (default, no args).
+        Pops the top value off the stack (no args -> default -> top item).
         :rtype: void
         """
-        print("Popping", self.stack[-1])
-        self.stack.pop() # Defaults to last item.
+        popped = self.stack[-1]
+        print("Popping", popped)
+        self.stack.pop()                        # Defaults to last item.
+
+        try:
+            if self.current_min is popped:
+                self.current_min = min(self.stack)
+                print("New Min:", self.current_min)
+        except:
+            self.current_min = None
+
+        self.length = len(self.stack)           # Update length using new data.
 ################################################################################
 
     def top(self):
@@ -41,25 +57,23 @@ class MinStack(object):
         Returns the top value of our stack.
         :rtype: int
         """
-        length = len(self.stack)
-        if length == 0:
+        if self.length == 0:
             return None
 
         print("Top Value:", self.stack[-1])
         return self.stack[-1]
 ################################################################################
 
-    def get_min(self):
+    def getMin(self):
         """
         Returns the smallest value in the stack.
         :rtype: int
         """
-        length = len(self.stack)
-        if length == 0:
+        if self.length == 0:
             return None
 
-        print("Min Value:", min(self.stack))
-        return min(self.stack)
+        print("Min Value:", self.current_min)
+        return self.current_min
 # MAIN #########################################################################
 
 # Your MinStack object will be instantiated and called as such:
@@ -72,4 +86,4 @@ OBJ.push(-3)
 OBJ.pop()
 
 PARAM_3 = OBJ.top()
-PARAM_4 = OBJ.get_min()
+PARAM_4 = OBJ.getMin()
